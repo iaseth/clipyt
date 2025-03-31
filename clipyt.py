332b2999a4cfb69865ee585c89587dd16c8ab794
Ankur Seth <iankurseth@gmail.com>
2025-03-31 09:44:13 +0530

    Now recording every 250ms

--- clipyt.py
+++ clipyt.py
@@ -14,6 +14,8 @@ db_path = os.path.join(app_data_dir, "clipyt.db")
 
 db = peewee.SqliteDatabase(db_path)
 
+WAIT_TIME = 1/4
+
 class ClipboardEntry(peewee.Model):
 	id = peewee.UUIDField(primary_key=True, default=uuid.uuid4)
 	timestamp = peewee.IntegerField()
@@ -22,24 +24,29 @@ class ClipboardEntry(peewee.Model):
 	class Meta:
 		database = db
 
+	@property
+	def date(self):
+		return datetime.fromtimestamp(self.timestamp)
+
 # Create table if not exists
 db.connect()
 db.create_tables([ClipboardEntry], safe=True)
 
 def record_clipboard():
-	last_clipboard = ""
+	last = ClipboardEntry.select().order_by(ClipboardEntry.timestamp.desc()).get()
+	last_clipboard = last.content if last else ""
 	while True:
 		current_clipboard = pyperclip.paste()
 		if current_clipboard and current_clipboard != last_clipboard:
-			ClipboardEntry.create(id=uuid.uuid4(), timestamp=int(time.time()), content=current_clipboard)
-			print(f"Recorded: {len(current_clipboard)} chars")
+			entry = ClipboardEntry.create(id=uuid.uuid4(), timestamp=int(time.time()), content=current_clipboard)
+			print(f"Recorded: {len(current_clipboard)} chars ({entry.date.time()})")
 			last_clipboard = current_clipboard
-		time.sleep(1)
+		time.sleep(WAIT_TIME)
 
 def list_entries():
 	entries = ClipboardEntry.select().order_by(ClipboardEntry.timestamp.desc()).limit(10)
 	for i, entry in enumerate(entries, start=1):
-		date = datetime.fromtimestamp(entry.timestamp)
+		date = entry.date
 		print(f"{i:4}. on {date.date()} at {date.time()} - {entry.content}")
 
 def main():
